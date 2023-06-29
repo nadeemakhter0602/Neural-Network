b18ab89a43a671a1a0765a824a2b69c5b530b181
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-06-29 22:03:32 +0530

    update softmax

--- layers/softmax.py
+++ layers/softmax.py
@@ -2,7 +2,11 @@ import numpy as np
 
 
 class Softmax():
-    def forward(self, inputs, axis=-1):
+    def __init__(self, axis=-1):
+        self.axis = axis
+
+    def forward(self, inputs):
+        axis = self.axis
         exponent = np.exp(inputs - np.max(inputs, axis=axis, keepdims=True))
         return exponent / exponent.sum(axis=axis, keepdims=True)
 
