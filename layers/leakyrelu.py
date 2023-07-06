a8d7bfe9cf76bb5e5557f433526ea20e53d33499
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-07-06 18:38:46 +0530

    remove __main__ leakyrelu

--- layers/leakyrelu.py
+++ layers/leakyrelu.py
@@ -8,12 +8,3 @@ class LeakyReLU:
     def forward(self, inputs):
         inputs[inputs < 0] *= self.alpha
         return inputs
-
-
-if __name__ == '__main__':
-    leakyrelu = LeakyReLU()
-    output = leakyrelu.forward(np.array([-3.0, -1.0, 0.0, 2.0]))
-    print(output)  # [-0.9, -0.3, 0.0, 2.0]
-    leakyrelu = LeakyReLU(alpha=0.1)
-    output = leakyrelu.forward(np.array([-3.0, -1.0, 0.0, 2.0]))
-    print(output)  # [-0.3, -0.1, 0.0, 2.0]
