9fb1355d619251859fe4a3a7905b061f9fe7f568
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-07-07 23:21:07 +0530

    add relu unit tests

---
+++ layers/relu_tests.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+from relu import ReLU
+
+
+class LeakyReLUTests(unittest.TestCase):
+    def setUp(self):
+        self.testing = np.testing
+
+    def test_relu(self):
+        layer = ReLU()
+        result = layer.forward(np.array([-3.0, -1.0, 0.0, 2.0]))
+        expected = np.array([0.0, 0.0, 0.0, 2.0])
+        self.testing.assert_array_almost_equal(expected, result)
+
+    def test_relu_with_max_value(self):
+        layer = ReLU(max_value=1.0)
+        result = layer.forward(np.array([-3.0, -1.0, 0.0, 2.0]))
+        expected = np.array([0.0, 0.0, 0.0, 1.0])
+        self.testing.assert_array_almost_equal(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
